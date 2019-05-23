88e7fd436140258ed06070bfaecce18899918747
Lucas Félix <lucas.salaverry@edu.pucrs.br>
2019-05-23 01:27:54 -0300

    Contextos Adicionados

---
+++ Quiz_Mario_Gauchada/FNB Mario Gauchada/Assets/Editor/DeletePlayerPrefs.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class DeletePlayerPrefs : MonoBehaviour {
+
+	[MenuItem("Edit/Reset Playerprefs")]
+	public static void DeletePlayerPref()
+	{
+		PlayerPrefs.DeleteAll();
+	}
+}
--- Quiz_Mario_Gauchada/FNB Mario Gauchada/Assets/Scripts/comandosBasicos.cs
+++ Quiz_Mario_Gauchada/FNB Mario Gauchada/Assets/Scripts/comandosBasicos.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
+
 public class comandosBasicos : MonoBehaviour {
+
+
+	public void Awake(){
+		Screen.SetResolution (384, 512, false);
+	}
 
 	public void carregaCena (string nomeCena)
 	{
 		SceneManager.LoadScene(nomeCena);
 	}
-
-
 
 
 }
